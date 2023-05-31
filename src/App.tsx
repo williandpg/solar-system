ccce4895aa07ac085fad072dd2ad25e22098baec
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-31 15:32:10 -0300

    Requisisto 5 finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 function App() {
   return (
---
+++ src/components/PlanetCard.tsx
@@ -0,0 +1,15 @@
+type PlanetCardProps = {
+  planetName: string;
+  planetImage: string;
+};
+
+function PlanetCard({ planetName, planetImage }: PlanetCardProps) {
+  return (
+    <div data-testid="planet-card">
+      <p data-testid="planet-name">{planetName}</p>
+      <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+    </div>
+  );
+}
+
+export default PlanetCard;
