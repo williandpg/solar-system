01aeacd76e5085d342459fc0b08dfcf51cbce8cb
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-31 15:35:26 -0300

    Requisisto 4 finalizado, tinha esquecido

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 function App() {
   return (
