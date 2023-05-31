2db572e8ce89aee29c0357e1fabf1ad1095e2a75
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-31 15:39:00 -0300

    Requisisto 6 finalizado

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -1,9 +1,18 @@
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <Title headline="Planetas" />
+      {planets.map((planet) => (
+        <PlanetCard
+          key={ planet.name }
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />
+      ))}
     </div>
   );
 }
