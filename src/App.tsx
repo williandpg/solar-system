1cef1bed24dd40de183ab050edb81727e2513f41
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-02 20:24:50 -0300

    finalizado faltando css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+
+import './App.css';
 
 function App() {
   return (
